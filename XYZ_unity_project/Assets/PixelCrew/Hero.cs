999c5c748c3ffbd2b44ac6c1d607de96967cd957
matsjov <matsjov@bk.ru>
2026-09-20 20:59:36 +0300

    project_init

---
+++ XYZ_unity_project/Assets/PixelCrew/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private float _direction;
+
+    [SerializeField] private float _speed;
+
+    public void SetDirection(float direction)
+    {
+        _direction = direction;
+    }
+
+    private void Update()
+    {
+        if (_direction != 0)
+        {
+            var delta = _direction * _speed * Time.deltaTime;
+            var newYPosition = transform.position.y + delta;
+            var newXPosition = transform.position.x + delta;
+            transform.position = new Vector3(newXPosition, newYPosition, transform.position.z);
+
+
+        }
+    }
+}
---
+++ XYZ_unity_project/Assets/PixelCrew/HeroInputReader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField]private Hero _hero;
+
+
+    //private void Update()
+    //{
+       
+
+    //    var horizontal = Input.GetAxis("Horizontal");
+    //    _hero.SetDirection(horizontal);
+
+    //    //Ниже легаси считывание клавиш
+
+    //    if (Input.GetKey(KeyCode.A))
+    //    {
+    //        _hero.SetDirection(-1);
+    //    }
+
+    //    else if (Input.GetKey(KeyCode.D))
+    //    {
+    //        _hero.SetDirection(1);
+    //    }
+
+    //    else
+    //    {
+    //        _hero.SetDirection(0);
+    //    }
+    //}
+
+    public void OnHorizontalMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirection(direction);
+    }
+
+    public void OnVerticalMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirection(direction);
+    }
+}
